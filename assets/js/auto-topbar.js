d42c79894bf00f9c0a46a6ff86e84e861ffbc775
cbx-nz <christmastvonlineclothes@gmail.com>
2026-01-13 11:34:50 +1300

    updated topbar

--- assets/js/auto-topbar.js
+++ assets/js/auto-topbar.js
@@ -2,11 +2,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const topbarHTML = `
     <div class="topbar">
-        <img class="logo" src="/assets/NBTF.png" alt="This is official NBTF Logo">
+        <div class="logo"></div>
         <div class="title">Nuclear Blast Testing Facility Wiki</div>
         <div class="nav-links">
-            <a href="https://nbtf.ca">NBTF Website</a>
             <a href="/index.html">Wiki Homepage</a>
+            <a href="https://nbtf.ca">NBTF Website</a>
             <a href="https://www.roblox.com/communities/12507282/Pyrowh#!/about">Group</a>
             <a href="https://discord.gg/nbtf">Discord</a>
             <a>   </a>
